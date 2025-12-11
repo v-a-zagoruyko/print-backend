d6c470eeb51e190fd81058803a49ffc86e2aa7b5
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-12-11 19:06:07 +0500

    Fix

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class MainConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -8,12 +8,19 @@ class MainConfig(AppConfig):
 
     def ready(self):
         from django.contrib import admin
-        from .models import BaseInfo
         from .utils.fonts import register_fonts
+
         register_fonts()
 
-        info = BaseInfo.get_solo()
-        if info:
-            admin.site.site_header = info.name
-            admin.site.site_title = info.name
-            admin.site.site_url = info.site_url
+        def set_admin_header(sender, **kwargs):
+            from .models import BaseInfo
+            try:
+                info = BaseInfo.get_solo()
+                if info:
+                    admin.site.site_header = info.name
+                    admin.site.site_title = info.name
+                    admin.site.site_url = info.site_url
+            except Exception:
+                pass
+
+        post_migrate.connect(set_admin_header, sender=self)

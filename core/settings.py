bc7da40192fb8ddc8c50b53fee73dffe0becd12c
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-12-11 19:41:06 +0500

    Fix prod cors

--- core/settings.py
+++ core/settings.py
@@ -165,11 +165,11 @@ if DEBUG:
     CSRF_COOKIE_SECURE = False
 else:
     SESSION_COOKIE_DOMAIN = ".большие-молодцы.рф"
-    SESSION_COOKIE_SECURE = True
+    # SESSION_COOKIE_SECURE = True
     # SESSION_COOKIE_SAMESITE = "None"
 
     CSRF_COOKIE_DOMAIN = ".большие-молодцы.рф"
-    CSRF_COOKIE_SECURE = True
+    # CSRF_COOKIE_SECURE = True
     # CSRF_COOKIE_SAMESITE = "None"
 
     CORS_ALLOW_HEADERS = list(default_headers) + ["Authorization"]

1010839cc1c1328da7ef60aff6d60c8eca25275b
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-12-11 21:02:42 +0500

    Fix prod cors

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 class MainConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -9,18 +8,15 @@ class MainConfig(AppConfig):
     def ready(self):
         from django.contrib import admin
         from .utils.fonts import register_fonts
+        from .models import BaseInfo
 
         register_fonts()
 
-        def set_admin_header(sender, **kwargs):
-            from .models import BaseInfo
-            try:
-                info = BaseInfo.get_solo()
-                if info:
-                    admin.site.site_header = info.name
-                    admin.site.site_title = info.name
-                    admin.site.site_url = info.site_url
-            except Exception:
-                pass
-
-        post_migrate.connect(set_admin_header, sender=self)
+        try:
+            info = BaseInfo.get_solo()
+            if info:
+                admin.site.site_header = info.name
+                admin.site.site_title = info.name
+                admin.site.site_url = info.site_url
+        except Exception:
+            pass
